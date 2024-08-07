cce404b7b59b81eb703bc3854f1177fb2f31dccb
jishnu2021 <jishnughosh98@gmail.com>
2024-08-07 19:45:13 +0530

    firstMissingPositive.java

---
+++ firstMissingPositive.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        
+        // Place each number in its right place
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                // Swap nums[i] with nums[nums[i] - 1]
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        
+        // Identify the first missing positive
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        
+        // If all numbers are in place, the first missing positive is n+1
+        return n + 1;
+    }
+}
