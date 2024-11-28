f8dc07b2f873f13fbbfe263423a6ff8b2bcb9d0e
jishnu2021 <jishnughosh98@gmail.com>
2024-11-28 10:48:48 +0530

    Kth largest Element

---
+++ KthLargestElement.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class KthLargestElement {
+
+    public static int findKthMax(int[] arr , int k){
+        for(int i = 0 ;i<arr.length;i++){
+            for(int j=1;j<arr.length-i;j++){
+                if(arr[j-1]<arr[j]){
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        int val=0;
+        for(int r = 0;r<k ;r++){
+            val = arr[r];
+        }
+        return val;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("The Kth value is : ");
+        int k = sc.nextInt();
+        System.out.print("The nth value is : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        int Kthmax = findKthMax(arr,k);
+        System.out.println(Kthmax);
+        sc.close();
+    }
+}
